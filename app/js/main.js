70ff69c8ffd2751549b6288c718ec2495d8b15d6
Chesy <chesy47@gmail.com>
2020-05-28 13:03:48 +0300

    Added page second

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 $(function(){
 
-    
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 15000,
+        max: 50000,
+        from: 15000,
+        to: 35000,
+    });
     
 });
