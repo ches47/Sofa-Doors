4b23670d5111bd6f4352f09d1fdabc3319a1403a
Chesy <chesy47@gmail.com>
2020-07-22 18:29:23 +0300

    80% done page Post

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,38 @@ $(function(){
         from: 15000,
         to: 35000,
     });
+
+
+    $('.slider-slick').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        dots: true,
+        nextArrow: false,
+        prevArrow: false,
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
     
+
 });
